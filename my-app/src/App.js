306593a36685dbc8682b946221c52bdd30c659ff
Shubham Mishra <shubhammishra78640@gmail.com>
2022-05-04 23:34:35 +0530

    updated

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Counter from './Components/Counter';
 
 function App() {
   return (
     <div className="App">
      <h1>hello</h1>
+     <Counter/>
     </div>
   );
 }
---
+++ my-app/src/Components/Counter.jsx
@@ -0,0 +1,10 @@
+
+function Counter() {
+  return (
+    <div className="App">
+     <h1>hello</h1>
+    </div>
+  );
+}
+
+export default Counter;
