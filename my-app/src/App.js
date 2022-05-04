aa369b0f35f8ac3bee0f991f1dd2b348a2db1d7b
Shubham Mishra <shubhammishra78640@gmail.com>
2022-05-05 00:14:05 +0530

    updated

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Counter from './Components/Counter';
-
+import Todo from './Components/Todo';
 function App() {
   return (
     <div className="App">
      <h1>hello</h1>
      <Counter/>
+     <Todo/>
     </div>
   );
 }
---
+++ my-app/src/Components/Todo.jsx
@@ -0,0 +1,10 @@
+function Todo() {
+    return (
+      <div className="App">
+       <h1>hello</h1>
+      <h1>Wlcm to todo list</h1>
+      </div>
+    );
+  }
+  
+  export default Todo;
