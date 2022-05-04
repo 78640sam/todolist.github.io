cdbf0e2ae57ffa18183aa52e047b0ff6c6abb61c
Shubham Mishra <shubhammishra78640@gmail.com>
2022-05-05 00:36:30 +0530

    updated

--- my-app/src/Components/Todo.jsx
+++ my-app/src/Components/Todo.jsx
@@ -1,8 +1,29 @@
+import { useState } from "react";
+
 function Todo() {
+var [data,setData]=useState("");
+var [data1,setData1]=useState([])
+  var formSubmit =()=>{
+ setData1(data)
+  }
     return (
       <div className="App">
        <h1>hello</h1>
-      <h1>Wlcm to todo list</h1>
+      <h1>Welcome to todo list</h1>
+      <form onSubmit={formSubmit}>
+        <input type="text" placeholder="TodoItem" value={data} onInput={(e)=>{
+         setData(e.target.value) ;
+        }}/>
+        <button > Add Item</button>
+      </form>
+      <ul>
+        
+        {
+          data1.map((item)=>{
+         return   <li>{item.data}</li>
+          })
+        }
+      </ul>
       </div>
     );
   }
