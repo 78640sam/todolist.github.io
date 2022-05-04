369d373f4aef48a6645e3670a41f39c933be58aa
Shubham Mishra <shubhammishra78640@gmail.com>
2022-05-04 23:53:07 +0530

    updated

--- my-app/src/Components/Counter.jsx
+++ my-app/src/Components/Counter.jsx
@@ -1,8 +1,24 @@
-
+import React, { useState } from 'react';
 function Counter() {
+    var [data,setData]=useState(0);
+
+    var adddata= (data)=>{
+        setData(data+1);
+        
+    }
+
+    var subdata= (data)=>{
+        setData(data-1);
+    }
+
   return (
     <div className="App">
      <h1>hello welcome yo counter page</h1>
+
+<h1>count={data}</h1>
+     <button onClick={adddata}>Add</button>
+     <button onClick={subdata}>Sub</button>
+
      
     </div>
   );
