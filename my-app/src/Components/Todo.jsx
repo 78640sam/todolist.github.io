fbb61f76d1d0f7bb07ebcfce40ddc33dc5338466
Shubham Mishra <shubhammishra78640@gmail.com>
2022-05-19 23:14:40 +0530

    hii

--- my-app/src/Components/Todo.jsx
+++ my-app/src/Components/Todo.jsx
@@ -19,10 +19,7 @@ var [data1,setData1]=useState([])
         <button > Add Item</button>
       </form>
    
-      <div>
-        <h1>hii</h1>
-        <h5>my name is sj</h5>
-      </div>
+    
       <ul>
         
         {
