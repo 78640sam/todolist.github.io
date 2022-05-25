d6614119f3f267373b0857ddb720954cc628ba33
Shubham Mishra <shubhammishra78640@gmail.com>
2022-05-25 23:34:55 +0530

    updated

--- my-app/src/Components/Todo.jsx
+++ my-app/src/Components/Todo.jsx
@@ -1,35 +1,35 @@
-import { useState } from "react";
+// import { useState } from "react";
 
-function Todo() {
-var [data,setData]=useState("");
-var [data1,setData1]=useState([]);
-  var formSubmit =()=>{
- setData1(data);
-  }
-    return (
-      <div className="App">
-       <h1>hii</h1>
-      <h1>Welcome to todo list</h1>
-      <h1>Hello i am Sam</h1>
-      <form onSubmit={formSubmit}>
-        <input type="text" placeholder="TodoItem" value={data} onInput={(e)=>{
-         setData(e.target.value);
-        }}/>
+// function Todo() {
+// var [data,setData]=useState("");
+// var [data1,setData1]=useState([]);
+//   var formSubmit =()=>{
+//  setData1(data);
+//   }
+//     return (
+//       <div className="App">
+//        <h1>hii</h1>
+//       <h1>Welcome to todo list</h1>
+//       <h1>Hello i am Sam</h1>
+//       <form onSubmit={formSubmit}>
+//         <input type="text" placeholder="TodoItem" value={data} onInput={(e)=>{
+//          setData(e.target.value);
+//         }}/>
 
-        <button > Add Item</button>
-      </form>
+//         <button > Add Item</button>
+//       </form>
    
     
-      <ul>
+//       <ul>
       
-        {
-          data1.map((item)=>{
-           <li>{item.data1}</li>
-          })
-        }
-      </ul>
-      </div>
-    );
-  }
+//         {
+//           data1.map((item)=>{
+//            <li>{item.data1}</li>
+//           })
+//         }
+//       </ul>
+//       </div>
+//     );
+//   }
   
-  export default Todo;
+//   export default Todo;
